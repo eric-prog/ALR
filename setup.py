7c720feeed43ad06b1c459c2aa24434e7dce64e1
Matthew3255 <112053337+Matthew3255@users.noreply.github.com>
2023-12-03 12:31:49 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+import os
+os.system("sudo apt update")
+os.system("sudo apt upgrade")
+os.system("python3 --version")
+os.system("sudo apt-get install python3-pip")
+os.system("pip3 install numpy")
+os.system("pip3 install opencv-python")
+os.system("pip3 install pyserial")
+os.system("sudo apt install fswebcam")
+os.system("sudo apt install linux-tools-virtual hwdata")
+os.system("sudo update-alternatives --install /usr/local/bin/usbip usbip `ls /usr/lib/linux-tools/*/usbip | tail -n1` 20")
+os.system("sudo apt upgrade")
